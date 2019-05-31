3a536a91577fc6c89bfa782b173646fedc14792e
Stephen Dickinson <stephencottontail@me.com>
2019-05-31 15:05:04 -0600

    Use MediaUpload instead of MediaPlaceholder
    I think this will end up being more flexible from a UI/UX standpoint
    in the long run, but this is mostly because I couldn't figure out how to get
    MediaPlaceholder to understand the distinction between creating a gallery
    and modifying an existing one

--- src/edit.js
+++ src/edit.js
@@ -1,5 +1,5 @@
 import { Component, createElement as el } from '@wordpress/element';
-import { MediaPlaceholder } from '@wordpress/block-editor';
+import { MediaUpload } from '@wordpress/block-editor';
 import Masonry from 'react-masonry-component';
 import classnames from 'classnames';
 
@@ -11,6 +11,19 @@ class DoubleMasonry extends Component {
     render() {
         const { attributes, setAttributes, className } = this.props;
         const hasImages = !! attributes.gallery.length;
+        const mediaUpload = el( MediaUpload,{
+            addToGallery: hasImages,
+            gallery: true,
+            multiple: true,
+            onSelect: ( img ) => {
+                setAttributes( { gallery: img } )
+            },
+            allowedTypes: [ 'image' ],
+            render: ( { open } ) => {
+                return el( 'button', { onClick: open }, 'Open' );
+            },
+            value: ( hasImages ? ( attributes.gallery.map( img => img.id ) ) : undefined )
+        } );
         if ( hasImages ) {
             let widths = attributes.gallery.reduce( ( smallest, img ) => {
                 return ( smallest < img.sizes.full.width ? smallest : img.sizes.full.width );
@@ -18,25 +31,12 @@ class DoubleMasonry extends Component {
         }
 
         return [
+            mediaUpload,
             ( hasImages && el( Masonry, {
                 className: classnames( className, { 'my-8': true } ),
             }, attributes.gallery.map( img => {
                 return el( 'div', { className: classnames( 'w-1/3', 'border', 'p-4' ) }, ( img.caption || img.url ) )
-            } ) ) ),
-            el( MediaPlaceholder, {
-                allowedTypes: [ 'image' ],
-                addToGallery: true,
-                gallery: true,
-                isAppender: true,
-                labels: {
-                    title: 'Gallery'
-                },
-                multiple: true,
-                onSelect: ( img ) => {
-                    setAttributes( { gallery: img } );
-                },
-                value: attributes.gallery.map( ( img ) => img.id )
-            } )
+            } ) ) )
         ];
     }
 }
