0fcae30487f1d5248210ed15a9855374c2157231
Stephen Dickinson <stephencottontail@me.com>
2019-06-03 12:15:04 -0600

    Images layout and resize correctly
    Note to self: set a sizing element if the first item in a Masonry
    layout might not be representative of the smallest brick or if it
    could change size in response to user input

--- src/edit.js
+++ src/edit.js
@@ -7,10 +7,44 @@ import classnames from 'classnames';
 class DoubleMasonry extends Component {
     constructor() {
         super( ...arguments );
+
+        this.checkBrickSize.bind( this );
+        this.createMasonryBrick.bind( this );
     }
 
     componentDidUpdate() {
-        console.log( 'the new width is ' + this.props.attributes.width );
+        this.masonry.layout();
+    }
+
+    checkBrickSize( element ) {
+        const { className } = this.props;
+        let referenceWidth = Math.round( this.props.attributes.width / 2 );
+        let smallest = this.props.attributes.gallery.reduce( ( smallest, img ) => {
+            return ( smallest < img.sizes.full.width ? smallest : img.sizes.full.width );
+        } );
+        let classes = [ `${className}__item`, 'p-4' ];
+
+        if ( smallest > referenceWidth ) {
+            classes.push( 'w-1/3' );
+            return classes;
+        }
+
+        if ( element.sizes.full.width < referenceWidth ) {
+            classes.push( 'w-1/3' );
+            return classes;
+        } else {
+            classes.push( 'w-2/3' );
+            return classes;
+        }
+    }
+
+    createMasonryBrick( element ) {
+        return el( 'div',
+                   { key: element.id.toString(), className: classnames( this.checkBrickSize( element ) ) },
+                   el( 'img', {
+                       alt: ( element.alt || undefined ),
+                       src: element.sizes.full.url
+                   } ) );
     }
 
     render() {
@@ -32,17 +66,16 @@ class DoubleMasonry extends Component {
             },
             value: ( hasImages ? ( attributes.gallery.map( img => img.id ) ) : undefined )
         } );
-        const masonry = el( Masonry, {
-            className: classnames( className, 'my-8' )
-        }, attributes.gallery.map( img => {
-            return el( 'div', { key: img.id.toString(), className: classnames( 'w-1/3', 'border', 'p-4' ) }, ( img.caption || img.url ) )
-        } ) );
-
-        if ( hasImages ) {
-            let widths = attributes.gallery.reduce( ( smallest, img ) => {
-                return ( smallest < img.sizes.full.width ? smallest : img.sizes.full.width );
-            } );
-        }
+        const masonry = el( Masonry,
+                            {
+                                ref: function( c ) { this.masonry = this.masonry || c.masonry; }.bind( this ),
+                                className: classnames( className, 'my-8' )
+                            },
+                            el( 'div', { className: `${className}__sizer` } ),
+                            attributes.gallery.map( img => {
+                                return this.createMasonryBrick( img );
+                            } )
+                          );
 
         return [
             mediaUpload,
--- src/masonry-frontend.js
+++ src/masonry-frontend.js
@@ -5,6 +5,7 @@
         container.imagesLoaded( function() {
             container.masonry( {
                 itemSelector: '.wp-block-sc-double-masonry__item',
+                columnWidth: '.wp-block-sc-double-masonry__sizer',
                 transitionDuration: 0.2,
                 percentPosition: true
             } );
